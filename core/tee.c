c9ea6bba15161de095e2862b9a2530f799825fbd
Michael Myer <misc.myer@zoho.com>
2022-05-10 18:06:03 +0000

    rewrite tee

--- core/tee.c
+++ core/tee.c
@@ -1,65 +1,61 @@
 
-//from: minutils
+void usage(){
+	writes("tee file1 file2 ... [-a file3 file4 ...]\n");
+	exit(0);
+}
 
-#include <fcntl.h>
-#include <unistd.h>
-
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/* Usage: tee [-a] [FILE...] */
 int main(int argc, const char *argv[])
 {
 	char buf[4096];
 	ssize_t n;
-	int i, ret, flags;
-	struct {
-		int *fds;
-		int len;
-	} files;
+	int flags = O_TRUNC;
+	int fds[MAXSPLIT];
+	fds[0] = STDOUT_FILENO;
+	int *fd = fds + 1;
 
-	if ( argc == 1 ){
-			writes("tee [-a] file1 file2 ...\n");
-			return(0);
+	if ( argc == 1 || (( argv[1][0] == '-' ) && (argv[1][1] == 'h')) ){
+		usage();
 	}
 
-	if (!strcmp(argv[1], "-a")) {
-		flags = O_APPEND;
-		files.len = argc - 1;
-		i = 2;
-	} else {
-		flags = O_TRUNC;
-		files.len = argc;
-		i = 1;
-	}
-
-	files.fds = malloc(files.len * sizeof(int));
-	if (!files.fds) {
-		fprintfs(stderr, "%s: unable to allocate: %s\n",
-			argv[0], strerror(errno));
-		return -1;
-	}
-	files.fds[0] = STDOUT_FILENO;
-	for (; i < argc; i++) {
-		files.fds[i] = open(argv[i], O_CREAT | O_WRONLY | flags, 0666);
-		if (files.fds[i] < 0)
-			return 1;
-	}
-	while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-		for (i = 0; i < argc; i++) {
-			if (write(files.fds[i], buf, n) != n) {
-				fprintfs(stderr, "%s: %s: %s\n",
-					argv[0], argv[i], strerror(errno));
-				return 1;
+	for ( *argv++; *argv; *argv++ ){
+		if ( argv[0][0] == '-' ){
+			switch ( argv[0][1] ) {
+				default:
+					eprintsl("Unknown option: ",argv[0]);
+				case 'h':
+					usage();
+				case 'a' :
+					flags = O_APPEND;
+			}
+		} else {
+			if ((*fd = open(argv[0], O_CREAT | O_WRONLY | flags, 0666))<0){
+				eprintsl("Couldn't open ", argv[0] );
+				exit_errno(*fd);
+			}
+			fd++;
+			if ( fd>=(fds+MAXSPLIT) ){
+				eprintsl("Too many splits");
+				exit(1);
 			}
 		}
 	}
+	*fd = 0;
 
-	//ret = 0;
-	for (i = 1; i < argc; i++)
-		close(files.fds[i]);
-	//free(files.fds);
-	return 0;
+	while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+		int *f = fds; 
+		do {
+			int e;
+			if ((e=write( *f, buf, n)) != n){ // write with splice to stdout ?
+				eprintsl(
+					"Couldn't open ", argv[0]);
+				exit_errno(e);
+			}
+			f++;
+		} while ( *f );
+	}
+
+	for ( int *f = (fds+1); *f; f++ )
+		close(*f);
+
+	exit(0);
 }
